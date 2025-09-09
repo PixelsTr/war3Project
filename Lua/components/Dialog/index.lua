302067c625e696e63ef6d335c926c180180605c8
lifang <fang.li@ehang.com>
2025-09-09 11:23:44 +0800

    Commit

--- Lua/components/Dialog/index.lua
+++ Lua/components/Dialog/index.lua
@@ -1,45 +1,13 @@
+-- Dialog moudle
+-- button(id) => dialog:show(id)
+-- 
+
 local Dialog = {}
 Dialog.__index = Dialog
 
--- 构造函数
+
 function Dialog:new(opts)
-    local self = setmetatable({}, Dialog)
-    self.title = opts.title or "标题"
-    self.content = opts.content or ""
-    self.onConfirm = opts.onConfirm
-    self.onCancel = opts.onCancel
+    local self = setmetatable({}, Dialog) -- 创建实例并绑定原型
+    self.title = opts.title or ""
     return self
-end
-
--- 展示方法
-function Dialog:show()
-    -- 创建一个War3对话框并展示（这里只是伪代码）
-    local dlg = DialogCreate()
-    DialogSetMessage(dlg, self.content)
-    DialogAddButton(dlg, "确定", 0)
-    DialogAddButton(dlg, "取消", 1)
-    DialogDisplay(GetLocalPlayer(), dlg, true)
-
-    TriggerRegisterDialogEvent(self:getTrigger(), dlg)
-end
-
--- 触发器处理
-function Dialog:getTrigger()
-    local trig = CreateTrigger()
-    TriggerAddAction(trig, function()
-        local btn = GetClickedButton()
-        local clicked = GetButtonText(btn)
-
-        if clicked == "确定" and self.onConfirm then
-            self.onConfirm()
-        elseif clicked == "取消" and self.onCancel then
-            self.onCancel()
-        end
-    end)
-    return trig
-end
-
--- 工厂方法
-return function(opts)
-    return Dialog:new(opts)
 end
